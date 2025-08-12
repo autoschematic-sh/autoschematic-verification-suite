6120d5d2183b0fbce8faaa6bc6999e1934d99f12
Peter Sherman <pfnsec@gmail.com>
2025-08-12 16:21:34 -0700

    feat: add full suite script

---
+++ run_suite.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -exo pipefail
+
+cargo run --bin autoschematic-testbench -- run --sequence sequences/unbundle.ron
+cargo run --bin autoschematic-testbench -- run --sequence sequences/import.ron
+cargo run --bin autoschematic-testbench -- run --sequence sequences/plan.ron
+cargo run --bin autoschematic-testbench -- run --sequence sequences/apply.ron
+
+git reset --hard
