a8393af765de09696eb5f67554c5f0c3a0cf6bed
Peter Sherman <pfnsec@gmail.com>
2025-12-01 22:28:09 -0800

    feat: enable task_exec in CI verification workflow

--- run_suite.sh
+++ run_suite.sh
@@ -5,3 +5,4 @@ cargo run --bin autoschematic-testbench -- run --sequence sequences/unbundle.ron
 cargo run --bin autoschematic-testbench -- run --sequence sequences/import.ron
 cargo run --bin autoschematic-testbench -- run --sequence sequences/plan.ron
 cargo run --bin autoschematic-testbench -- run --sequence sequences/apply.ron
+cargo run --bin autoschematic-testbench -- run --sequence sequences/task_exec.ron
